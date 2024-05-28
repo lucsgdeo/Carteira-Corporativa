4d5a3827e897d40732bdff687bf55ff9a9cd3d62
Aluno <Aluno@fatecdiadema.com>
2024-05-28 10:49:53 -0300

    Adicao de pagiginas

--- index.js
+++ index.js
@@ -13,19 +13,45 @@ app.get("/", (req, res) => { //Redireciona para a página index
     res.redirect(301, "/index");
 })
 
-app.get("/index", function(req,res){
-    res.sendFile(path.join(__dirname, './public', 'index.html'));   
+app.get("/absenteimos", function(req,res){
+    res.sendFile(path.join(__dirname, './public', 'absenteimos.html'));   
 })
-
-
+app.get("/beneficiosSecundarios", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "beneficiosSecundarios.html"))
+})
 app.get("/cadastro", function(req,res){
 	res.sendFile(path.join(__dirname, "./public", "cadastro.html"))
 })
-
+app.get("/ferias", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "ferias.html"))
+})
+app.get("/historicoHolerite", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "historicoHolerite.html"))
+})
+app.get("/historicoPonto", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "historicoPonto.html"))
+})
+app.get("/holerite", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "holerite.html"))
+})
+app.get("/index", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "index.html"))
+})
 app.get("/infoPessoais", function(req,res){
 	res.sendFile(path.join(__dirname, "./public", "infoPessoais.html"))
 })
-
-app.get("/historicoPonto", function(req,res){
-	res.sendFile(path.join(__dirname, "./public", "historicoPonto.html"))
+app.get("/noticias", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "noticias.html"))
+})
+app.get("/noticiasRH", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "noticiasRH.html"))
+})
+app.get("/vagasInternas", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "vagasInternas.html"))
+})
+app.get("/valeAlimentacao", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "valeAlimentacao.html"))
+})
+app.get("/valeTransporte", function(req,res){
+	res.sendFile(path.join(__dirname, "./public", "valeTransporte.html"))
 })
